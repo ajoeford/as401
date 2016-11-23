50606dc90eb116f0a7e6cce56ea3252301f6615d
AJ <lebronaldreagan@hotmail.com>
2016-11-22 21:50:42 -0800

    base of chart of accounts module finished.

--- as401v1.py
+++ as401v1.py
@@ -27,8 +27,14 @@ class TBAccount(object):
     def get_acct_name(self):
         return self.acct_name
 
-    def get_classification(self):
-        return self.classification
+    def get_acct_classification(self):
+        return self.acct_classification
+
+    def set_acct_name(self, new_name):
+        self.acct_name = new_name
+
+    def set_acct_classification(self, new_classification):
+        self.acct_classification = new_classification
 
 
 class JournalPiece(object):
@@ -88,7 +94,7 @@ class JournalEntry(object):
         out = str(self.number)
 
 
-def Aje_module():
+def aje_module():
     aje_running = True
 
     while aje_running:
@@ -101,7 +107,7 @@ def Aje_module():
         if user_input == "X":
             aje_running = False
 
-def Create_Account():
+def create_account():
     """Add an account to the TB"""
     global chart_of_accounts
     create_account_running = True
@@ -111,8 +117,17 @@ def Create_Account():
         print "CREATE ACCOUNT!!!"
         print ""
 
-        print "Enter account number: "
-        acct_num = raw_input(" ")
+        num_loop = True
+        while num_loop:
+            print "Enter account number: "
+            acct_num = raw_input(" ")
+
+            for account in chart_of_accounts:
+                if account.get_acct_num() == acct_num:
+                    print "Account number already exists."
+                    break
+            else:
+                num_loop = False
 
         print "Enter account name: "
         acct_name = raw_input(" ")
@@ -140,6 +155,71 @@ def Create_Account():
             if continue_prompt == 'N':
                 unaccepted = False
 
+def edit_account():
+    """Change account name or classification based on account number"""
+    global chart_of_accounts
+    valid_account = ""
+
+    acct_input = raw_input("Enter account number: ")
+    for account in chart_of_accounts:
+        if account.get_acct_num() == acct_input:
+            valid_account = account
+            break
+
+    if valid_account != "":
+        print(valid_account.get_acct_num() + " "+ valid_account.get_acct_name())
+        print("Classification: " + valid_account.get_acct_classification())
+
+        edited = False
+        while edited == False:
+            ask_name = raw_input("Edit account name? (Y/N) ")
+
+            if ask_name == 'Y' or ask_name == 'y':
+                new_name = raw_input("Enter new name: ")
+
+                valid_account.set_acct_name(new_name)
+                edited = True
+
+            ask_class = raw_input("Change account classification? (Y/N) ")
+
+            if ask_class == 'Y' or ask_class == 'y':
+
+                acct_classification = ""
+                while acct_classification not in ["A", "L", "OE", "R", "E"]:
+
+                    print "Enter account classification (A/L/OE/R/E)"
+                    acct_classification = raw_input("Classification: ")
+
+                valid_account.set_acct_classification(acct_classification)
+                edited = True
+
+            print(valid_account.get_acct_num() + " "+ valid_account.get_acct_name())
+            print("Classification: " + valid_account.get_acct_classification())
+    else:
+        print("Invalid account number.")
+
+def delete_account():
+    """Remove account from Chart of Accounts"""
+    global chart_of_accounts
+    to_delete = ""
+
+    print("")
+    delete_num = raw_input("Enter account number: ")
+
+    for account in chart_of_accounts:
+        if account.get_acct_num() == delete_num:
+            to_delete = account
+
+    if to_delete == "":
+        print("Invalid account number.")
+
+    else:
+        print("Are you sure you want to delete the following account?")
+        print(to_delete.get_acct_num() + " "+ to_delete.get_acct_name())
+        affirm = raw_input("Delete? (Y/N) ")
+        if affirm == 'Y' or affirm == 'y':
+            chart_of_accounts.remove(to_delete)
+
 def Chart_module():
     global chart_of_accounts
     chart_running = True
@@ -161,7 +241,10 @@ def Chart_module():
             chart_running = False
 
         elif user_input == "1":
-            Create_Account()
+            create_account()
+
+        elif user_input == "2":
+            edit_account()
 
         elif user_input == "3":
             print ""
@@ -171,6 +254,9 @@ def Chart_module():
 
             for account in chart_of_accounts:
                 print account.get_acct_num() + ' ' + account.get_acct_name()
+
+        elif user_input == "4":
+            delete_account()
 
 def Main():
     global running
@@ -186,7 +272,7 @@ def Main():
         running = False
 
     elif user_input == '1':
-        Aje_module()
+        aje_module()
     elif user_input == '3':
         Chart_module()
 
