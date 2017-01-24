b693f539be4f3ec04db49e75eb882e326ed887f4
AJ <lebronaldreagan@hotmail.com>
2017-01-23 22:17:11 -0800

    Added print gl by begin and end dates.

--- reporting.py
+++ reporting.py
@@ -20,6 +20,26 @@ def get_account_gl(acct_num, dbcon):
 
     return gl_pieces
 
+def get_account_gl_dated(acct_num, date_b, date_e, dbcon):
+    """
+    Takes TB account number, begin date, end date; returns gl for that period
+    Parameters: String acct_num, Datetime.date date_b, Datetime.date date_e
+    returns: list of GL lines
+    """
+    db = DBManagerDatetime(dbcon)
+
+    #add a day to end date and then convert to Datetime
+    #this allows proper sql comparison of datetimes
+    date_e += datetime.timedelta(days=1)
+    datetime_e = datetime.datetime(date_e.year, date_e.month, date_e.day)
+    #convert begin date to datetime
+    datetime_b = datetime.datetime(date_b.year, date_b.month, date_b.day)
+
+    db.query("SELECT * FROM gl WHERE account=? AND entry_date>=? AND entry_date<?"
+        ,(acct_num, datetime_b, datetime_e))
+
+    return db.fetchall()
+
 def get_account_balance(acct_num, dbcon):
     """
     Summary: Takes TB account number, returns TB account balance
@@ -82,6 +102,34 @@ def view_account_gl_prompt(dbcon):
     else:
         print("TB account not found.")
 
+def view_gl_by_date_prompt(dbcon):
+    """
+    Asks for account, date range, prints GL detail of account
+    """
+    account_query = int(raw_input("Enter Account #: "))
+
+    if account_exists(account_query, dbcon):
+
+        input_begindate = raw_input("Enter start date (MM/DD/YYYY): ")
+        input_enddate = raw_input("Enter end date (MM/DD/YYYY): ")
+
+        #convert user input to datetime.date
+        try:
+            begindate_formatted = convert_string_date(input_begindate)
+            enddate_formatted = convert_string_date(input_enddate)
+
+            gl_lines = get_account_gl_dated(account_query,begindate_formatted,
+                enddate_formatted, dbcon)
+
+            for line in gl_lines:
+                print(ajemodule.print_piece(line, dbcon))
+
+        except ValueError:
+            print("Improper date format.")
+
+    else:
+        print("TB account not found.")
+
 def view_balance_prompt(dbcon):
     """
     Prints TB account, description, and balance
@@ -115,17 +163,17 @@ def view_balance_date_prompt(dbcon):
         #convert user input to datetime.date
         try:
             date_formatted = convert_string_date(date_query)
+
+            #get account description
+            acct_description = get_acct_description(account_query,dbcon)
+
+            #get account total
+            acct_balance = get_account_balance_dated(date_formatted,account_query,dbcon)
+
+            print(str(account_query)+" "+acct_description+" "+decify(acct_balance))
+
         except ValueError:
             print("Improper date format.")
-            return null
-
-        #get account description
-        acct_description = get_acct_description(account_query,dbcon)
-
-        #get account total
-        acct_balance = get_account_balance_dated(date_formatted,account_query,dbcon)
-
-        print(str(account_query)+" "+acct_description+" "+decify(acct_balance))
 
     else:
         print("TB account not found.")
@@ -139,6 +187,7 @@ def reporting_module(dbcon):
         print "1) View Account Balance"
         print "2) View Account Balance at Date"
         print "3) View Account Detail"
+        print "4) View Account Detail (Dated)"
         print "6) View TB"
         print "X) Back\n"
 
@@ -155,6 +204,9 @@ def reporting_module(dbcon):
 
         elif user_input == "3":
             view_account_gl_prompt(dbcon)
+
+        elif user_input == "4":
+            view_gl_by_date_prompt(dbcon)
 
         elif user_input == "8":
             pass
